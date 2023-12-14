b7d4f83a03a46775ec803d5ae8596b32cda7c187
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2023-12-14 20:24:48 +0100

    Changed header font & cookies tab

--- script.js
+++ script.js
@@ -61,3 +61,125 @@ tabsContainer.addEventListener('click', function (e) {
     document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 });
 
+const nav = document.querySelector('.nav');
+
+const handleMenuHover = function (e) {
+    if (e.target.classList.contains('nav__link')) {
+        const link = e.target;
+        const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+        const logo = link.closest('.nav').querySelector('img');
+        siblings.forEach(sibling => {
+            if (sibling !== link) sibling.style.opacity = this;
+        });
+        logo.style.opacity = this;
+    }
+};
+
+nav.addEventListener('mouseover', handleMenuHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleMenuHover.bind(1));
+
+const headerSection = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+const stickyNav = function (entries) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+});
+headerObserver.observe(headerSection);
+
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+    root: null,
+    threshold: 0.15
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+});
+
+const targetImages = document.querySelectorAll('img[data-src]');
+
+const loading = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) return;
+    entry.target.src = entry.target.dataset.src;
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img');
+    });
+    observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '-100px'
+});
+
+targetImages.forEach(img => imgObserver.observe(img));
+
+const slider = function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const maxSlide = slides.length;
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+
+    const goToSlide = function (slide) {
+    slides.forEach(
+        (slide, index) =>
+        (slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`)
+    );
+    };
+    goToSlide(0);
+    const nextSlide = function () {
+    if (currentSlide === maxSlide - 1) {
+        currentSlide = 0;
+    } else currentSlide++;
+
+    goToSlide(currentSlide);
+    };
+
+    const prevSlide = function () {
+        if (currentSlide === 0) {
+        currentSlide = maxSlide - 1;
+        } else {
+        currentSlide--;
+        }
+        goToSlide(currentSlide);
+    };
+
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', prevSlide);
+
+    document.addEventListener('click', nextSlide);
+    document.addEventListener('click', prevSlide);
+
+    document.addEventListener('keydown', function (e) {
+        if (e.key === 'ArrowLeft') prevSlide();
+        e.key === 'ArrowRight' && nextSlide();
+    });
+
+    dotContainer.addEventListener('click', function (e) {
+        if (e.target.classList.contains('dots__dot')) {
+            const { slide } = e.target.dataset;
+            goToSlide(slide);
+            activeDot(slide);
+        }
+    });
+};
+
+slider();
